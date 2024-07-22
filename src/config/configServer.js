dceafd69ca1b4f35f1dee0b77d4b163168d73b4b
juancruz92 <g.juancruz@gmail.com>
2024-07-22 10:50:19 -0300

    deploy fixing

--- src/config/configServer.js
+++ src/config/configServer.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 import { loggerDev } from "./logger.js";
-
-// Asegúrate de que esta URI esté configurada en tus variables de entorno
-const URI = config.MONGO_URL
+const URI = config.MONGO_URL;
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(URI, {
-      tls: true,  // Habilitar TLS/SSL
-      ssl: true,
-      sslValidate: true,
-    });
+    await mongoose.connect(URI);
     loggerDev.info("Connected to DB ecommerce");
   } catch (error) {
     loggerDev.error("Error connecting to DB ecommerce", error);

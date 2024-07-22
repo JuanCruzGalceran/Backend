025494d81cfa0c186173cadc7ec2222438c57160
juancruz92 <g.juancruz@gmail.com>
2024-07-22 10:38:43 -0300

    configuracion de mongo actualizada

--- src/config/configServer.js
+++ src/config/configServer.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 import { loggerDev } from "./logger.js";
 
-const URI = "mongodb+srv://gjuancruz:juankadel77@cluster0.hspesbp.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0";
-const connectToDB = () => {
+// Asegúrate de que esta URI esté configurada en tus variables de entorno
+const URI = process.env.MONGO_URL || "mongodb+srv://gjuancruz:juankadel77@cluster0.hspesbp.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0";
+
+const connectToDB = async () => {
   try {
-    mongoose.connect(URI);
-    loggerDev.info("connected to DB ecommerce");
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,  // Habilitar TLS/SSL
+      tlsCAFile: '/path/to/ca.pem',  // Ruta al certificado CA si es necesario
+    });
+    loggerDev.info("Connected to DB ecommerce");
   } catch (error) {
-    loggerDev.error(error);
+    loggerDev.error("Error connecting to DB ecommerce", error);
   }
 };
 
